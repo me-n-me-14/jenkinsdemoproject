2f9e37c67d6e3ba3b59b236aeae4bded94d74532
me-n-me-14 <112001124+me-n-me-14@users.noreply.github.com>
2025-08-01 03:03:15 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    dice_result = None
+    if request.method == 'POST':
+        dice_result = random.randint(1, 6)
+    return render_template('index.html', dice=dice_result)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
